c880c733edfa39ace17b84662ee88a105262b2fd
Loïc <Loc@TYPICAL-PC>
2014-10-09 20:43:34 +0200

    Just the window

---
+++ Java3D/src/fr/view/Window.java
@@ -0,0 +1,15 @@
+package fr.view;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+	public Window() {
+		super("3D Lib");
+		//this.setIconImage(new ImageIcon("rsc/images/icon.png").getImage());
+		//this.getContentPane().add();
+		this.setBounds(200,80,800,600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+}
