37138ef446be4cada36af8554823b67905b4a42f
Loïc <Loc@TYPICAL-PC>
2014-10-09 17:29:10 +0200

    Structuration du projet sous la forme MVC (modele-vue-controleur)

---
+++ Java3D/src/fr/model/ReadText.java
@@ -0,0 +1,13 @@
+package fr.model;
+
+/**
+ * @author Loïc
+ * Cette classe a pour but de lire un fichier texte contenant le code d'un objet et de le parser
+ * puis de le ranger dans une ArrayList.
+ */
+public class ReadText {
+
+
+	public ReadText(){
+	}
+}
