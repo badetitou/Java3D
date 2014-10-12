e32828b22b7a30973713ad2832808c9b15a2cfc6
Benoît <badetitou@gmail.com>
2014-10-12 12:40:34 +0200

    Creation methode rotation X

--- Java3D/src/fr/model/Matrice.java
+++ Java3D/src/fr/model/Matrice.java
@@ -3,11 +3,43 @@ package fr.model;
 import Jama.Matrix;
 
 public class Matrice {
-	Matrix points;
+	private Matrix points;
 	
 	public Matrice(double[][] ensemblePoints){
 		points = new Matrix(ensemblePoints);
 	}
 	
+	/**
+	 * 
+	 * @return la matrice avec les points
+	 */
+	public Matrix getMatrice(){
+		return points;
+	}
+	
+	/**
+	 * EN COUR
+	 * 
+	 * @author Benoit
+	 * @param rotation
+	 * @return la nouvelle matrice après rotation sur l'axe x de rotation
+	 */
+	public Matrix rotationX(double rotation){
+		Matrix c = new Matrix(points.getColumnDimension(), points.getRowDimension()+1);
+		for(int i = 0;i<points.getColumnDimension();++i){
+			for(int j = 0;j<points.getRowDimension();++j){
+				c.set(i, j, points.get(i, j));
+			}
+		}
+		for(int i = 0;i<points.getColumnDimension();++i){
+			for(int j = 0;j<points.getRowDimension()+1;++j){
+				c.set(i, j, 1);
+			}
+		}
+		
+		double[][] tableauRotation = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+		
+		return points;
+	}
 	
 }
