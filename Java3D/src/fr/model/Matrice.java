6d58c3a12952f80064f330cdc456e5e81bebeab0
Benoît <badetitou@gmail.com>
2014-10-12 15:19:44 +0200

    Ensemble des rotations

--- Java3D/src/fr/model/Matrice.java
+++ Java3D/src/fr/model/Matrice.java
@@ -44,4 +44,59 @@ public class Matrice {
 		return points.arrayTimesEquals(matriceRotation);
 	}
 
+	/**
+	 * 
+	 * @author Benoit
+	 * @param rotation
+	 * @return la nouvelle matrice après rotation sur l'axe y de rotation
+	 */
+	public Matrix rotationY(double rotation) {
+		Matrix c = new Matrix(points.getColumnDimension(),
+				points.getRowDimension() + 1);
+		for (int i = 0; i < points.getColumnDimension(); ++i) {
+			for (int j = 0; j < points.getRowDimension(); ++j) {
+				c.set(i, j, points.get(i, j));
+			}
+		}
+		for (int i = 0; i < points.getColumnDimension(); ++i) {
+			for (int j = 0; j < points.getRowDimension() + 1; ++j) {
+				c.set(i, j, 1);
+			}
+		}
+		double[][] tableauRotation = { 
+				{ Math.cos(rotation), 0, Math.sin(rotation), 0 },
+				{ 0, 1, 0, 0 },
+				{ -Math.sin(rotation), 0, Math.cos(rotation), 0 },
+				{ 0, 0, 0, 1 } };
+		Matrix matriceRotation = new Matrix(tableauRotation);
+		return points.arrayTimesEquals(matriceRotation);
+	}
+	
+	/**
+	 * 
+	 * @author Benoit
+	 * @param rotation
+	 * @return la nouvelle matrice après rotation sur l'axe z de rotation
+	 */
+	public Matrix rotationZ(double rotation) {
+		Matrix c = new Matrix(points.getColumnDimension(),
+				points.getRowDimension() + 1);
+		for (int i = 0; i < points.getColumnDimension(); ++i) {
+			for (int j = 0; j < points.getRowDimension(); ++j) {
+				c.set(i, j, points.get(i, j));
+			}
+		}
+		for (int i = 0; i < points.getColumnDimension(); ++i) {
+			for (int j = 0; j < points.getRowDimension() + 1; ++j) {
+				c.set(i, j, 1);
+			}
+		}
+		double[][] tableauRotation = { 
+				{ Math.cos(rotation), -Math.sin(rotation),0, 0 },
+				{ Math.sin(rotation), Math.cos(rotation), 0, 0 },
+				{ 0, 0, 1, 0 },
+				{ 0, 0, 0, 1 } };
+		Matrix matriceRotation = new Matrix(tableauRotation);
+		return points.arrayTimesEquals(matriceRotation);
+	}
 }
