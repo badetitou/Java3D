2070ac1a61ceeb4292025e13e7635766bd4f73f9
Loïc <Loc@TYPICAL-PC>
2014-10-13 20:26:04 +0200

    Creation de la classe face et point afin de pouvoir stocker les données en entrée

---
+++ Java3D/src/fr/model/Point.java
@@ -0,0 +1,12 @@
+package fr.model;
+
+public class Point {
+	private int x;
+	private int y;
+	private int z;
+	public Point(int x, int y, int z){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+}
--- Java3D/src/fr/model/ReadText.java
+++ Java3D/src/fr/model/ReadText.java
@@ -2,8 +2,8 @@ package fr.model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,21 +12,43 @@ import java.util.Scanner;
  */
 public class ReadText {
 
-	private final Map<Integer,Integer[]> map;
+	private final List<Point> pointList;
+	private final List<Face> faceList;
 	private Scanner scanner;
 	private final String file;
 	@SuppressWarnings("resource")
 	public ReadText(String file) {
-		map=new HashMap<Integer,Integer[]>();
+		pointList=new ArrayList<Point>();
+		faceList=new ArrayList<Face>();
 		this.file=file;
 		try {
 			scanner = new Scanner(new File(file));
+			int i=0;
+			int nbPoints=0;
+			int nbSegments=0;
+			int nbFaces=0;
+			String line="";
 			// On boucle sur chaque champ detecté
 			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
+				line = scanner.nextLine();
+				if (i==0){
+					nbPoints=Integer.parseInt(line.substring(0));
+					nbSegments=Integer.parseInt(line.substring(2));
+					nbFaces=Integer.parseInt(line.substring(4));
+				}
 
-				System.out.println(line);
-				//faites ici votre traitement
+				else if (i<=nbPoints){
+					pointList.add(new Point(Integer.parseInt(line.substring(0)), Integer.parseInt(line.substring(2)), Integer.parseInt(line.substring(4))));
+				}
+
+				else if (i> nbPoints && i<= nbSegments){
+
+				}
+
+				else {
+
+				}
+				i++;
 			}
 
 			scanner.close();
@@ -41,8 +63,12 @@ public class ReadText {
 		return this.file;
 	}
 
-	public Map<Integer,Integer[]> getMap(){
-		return this.map;
+	public List<Point> getPointList(){
+		return this.pointList;
+	}
+
+	public List<Face> getFaceList(){
+		return this.faceList;
 	}
 
 }
