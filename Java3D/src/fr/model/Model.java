efd54bdbce43604035cb638de7c9c4ee02af7c5c
Benoît <badetitou@gmail.com>
2014-10-14 18:26:34 +0200

    Ajout rotation en Y test 1

--- Java3D/src/fr/model/Model.java
+++ Java3D/src/fr/model/Model.java
@@ -14,10 +14,19 @@ public class Model {
 
 	public void rotationX(double r) {
 		for (int i = 0; i < rt.getPointList().size(); ++i) {
-			rt.getPointList().get(i).multiplier(
-							new double[][] { { 1, 0, 0 },
-									{ 0, Math.cos(r), -Math.sin(r) },
-									{ 0, Math.sin(r), Math.cos(r) } });
+			rt.getPointList().get(i).multiplier(new double[][] { 
+					{ 1, 0, 0 },
+					{ 0, Math.cos(r), -Math.sin(r) },
+					{ 0, Math.sin(r), Math.cos(r) } });
+		}
+	}
+
+	public void rotationY(double r) {
+		for (int i = 0; i < rt.getPointList().size(); ++i) {
+			rt.getPointList().get(i).multiplier(new double[][] { 
+					{ Math.cos(r), 0, Math.sin(r) },
+					{ 0, 1, 0 },
+					{ -Math.sin(r), 0, Math.cos(r) } });
 		}
 	}
 
