6cadc57ff9ee8ea9a1d86ffd17d1ad5733517b16
Loïc <Loc@TYPICAL-PC>
2014-10-16 16:28:08 +0200

    amélioration progressbar

--- Java3D/src/fr/view/ProgressBar.java
+++ Java3D/src/fr/view/ProgressBar.java
@@ -1,5 +1,7 @@
 package fr.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 
@@ -9,7 +11,7 @@ import javax.swing.Timer;
 
 public class ProgressBar extends JPanel{
 	private static JProgressBar progressBar = new JProgressBar();
-	private static int count = 1, PROGBAR_MAX=100,TIMER_PAUSE = 25;
+	private static int count = 1, PROGBAR_MAX=100,TIMER_PAUSE = 20;
 	private static SplashScreen ss;
 	private static Timer progressBarTimer;
 	ActionListener al = new ActionListener() {
@@ -21,14 +23,20 @@ public class ProgressBar extends JPanel{
 				progressBarTimer.stop();//stop the timer
 				Window w = new Window();
 			}
+			progressBar.setStringPainted(true);
+			progressBar.setString(count+"%");
 			count++;//increase counter
 
 		}
 	};
 	public ProgressBar(SplashScreen ss){
 		progressBar.setMaximum(PROGBAR_MAX);
+		progressBar.setBorderPainted(false);
+		progressBar.setBackground(Color.lightGray);
+		progressBar.setPreferredSize(new Dimension(180,18));
+		//progressBar.setForeground(Color.DARK_GRAY);
 		this.ss=ss;
-		this.setLayout(new FlowLayout(0,10,350));
+		this.setLayout(new FlowLayout(0,0,350));
 		this.setOpaque(false);
 		this.add(progressBar);
 		startProgressBar();
--- Java3D/src/fr/view/SplashScreenPanel.java
+++ Java3D/src/fr/view/SplashScreenPanel.java
@@ -1,5 +1,6 @@
 package fr.view;
 
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
@@ -18,7 +19,7 @@ public class SplashScreenPanel extends JPanel{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//	this.setLayout(new GridLayout(3,2));
+		this.setLayout(new FlowLayout(0,80,10));
 		ProgressBar progressBar = new ProgressBar(ss);
 		this.add(progressBar);
 	}
