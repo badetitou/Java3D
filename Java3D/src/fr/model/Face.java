15b04b6d8ae8839ef9a4b7dacf1b6be2eb42026e
Benoît <badetitou@gmail.com>
2014-10-17 16:58:35 +0200

    Finish epic color

--- Java3D/src/fr/model/Face.java
+++ Java3D/src/fr/model/Face.java
@@ -17,7 +17,7 @@ public class Face implements Comparable<Face> {
 	private final Point p1;
 	private final Point p2;
 	private final Point p3;
-	private Color color=Color.BLUE;
+	private Color color=Color.BLACK;
 	public static int i = 0;
 
 	public Face(Point p1, Point p2, Point p3){
@@ -61,18 +61,6 @@ public class Face implements Comparable<Face> {
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(this.color);
-		Random r = new Random();
-		int i = r.nextInt(300);
-		if(i < 50 )
-			g2.setColor(Color.RED);
-		else if(i < 100 )
-			g2.setColor(Color.YELLOW);
-		else if (i<150)
-			g2.setColor(Color.DARK_GRAY);
-		else if(i<200)
-			g2.setColor(Color.CYAN);
-		else if(i<250)
-			g2.setColor(Color.GREEN);
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 		GeneralPath p0 = new GeneralPath();
