e2a2d54274b48bd1b6711ca0b002cc69e0334637
Loïc <Loc@TYPICAL-PC.home>
2014-10-29 21:09:35 +0100

    Galerie d'images opérationnelles

--- Java3D/src/fr/view/PanelImages.java
+++ Java3D/src/fr/view/PanelImages.java
@@ -2,6 +2,7 @@ package fr.view;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.util.ArrayList;
 
@@ -16,28 +17,25 @@ public class PanelImages extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public static int columns=35;
+	public static int columns=55;
 	private final JPanel galerie;
 	private final ArrayList<JLabel> images;
 	private String url="";
 	public PanelImages(){
 		this.setLayout(new FlowLayout(0,20,20));
 		galerie=new JPanel();
-		galerie.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/5));
-		galerie.setLayout(new FlowLayout(0,5,5));
-		//galerie.setLayout(new GridLayout(1,columns,7,0));
+		galerie.setLayout(new GridLayout((columns/7)+1,7,10,10));
 		images = new ArrayList<JLabel>();
-		JScrollPane scroll = new JScrollPane(galerie);
-		scroll.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/5));
 		JLabel label;
 		for (int i=0;i< columns;i++){
 			label=new JLabel();
-			url="ressources/icones/oneview.png";
-			label.setIcon(new ImageIcon(new ImageIcon(url).getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
+			url="ressources/icones/iconerota.png";
+			label.setIcon(new ImageIcon(new ImageIcon(url).getImage().getScaledInstance(Window.outil.getScreenSize().width/10, Window.outil.getScreenSize().width/10, Image.SCALE_DEFAULT)));
 			images.add(label);
 			galerie.add(images.get(i));
 		}
-		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		JScrollPane scroll = new JScrollPane(galerie);
+		scroll.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/5));
 		this.add(scroll);
 
 
