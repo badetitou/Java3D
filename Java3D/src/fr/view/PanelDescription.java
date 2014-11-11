6c1505dca65eefd986735e3360da2c02a7de6de9
Loïc <Loc@TYPICAL-PC>
2014-11-11 17:33:51 +0100

    Resolution du probleme d'affichage plein écran :)

--- Java3D/src/fr/view/PanelDescription.java
+++ Java3D/src/fr/view/PanelDescription.java
@@ -36,7 +36,7 @@ public class PanelDescription extends JPanel implements MouseListener{
 
 
 		textArea=new JTextArea();
-		textArea.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/6));
+		textArea.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/5));
 		textArea.setEditable(false);
 		textArea.setBackground(new Color(230,230,230));
 
