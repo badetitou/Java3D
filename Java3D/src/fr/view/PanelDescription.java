16de5f12cac1bfac505dce9d9d515e5503959ede
Loïc <Loc@TYPICAL-PC>
2014-11-11 17:14:22 +0100

    Suppression de l'axe Z mais problème lié à la rotation lorsque x et y ne sont pas activés, + description gérée

--- Java3D/src/fr/view/PanelDescription.java
+++ Java3D/src/fr/view/PanelDescription.java
@@ -4,13 +4,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class PanelDescription extends JPanel {
+public class PanelDescription extends JPanel implements MouseListener{
 
 	/**
 	 * 
@@ -22,8 +24,7 @@ public class PanelDescription extends JPanel {
 	public JPanel panelDescription;
 
 	private final JButton valider;
-	private final JButton supprimer;
-	private final JButton annuler;
+	private final JButton modifier;
 
 	private final JLabel label;
 
@@ -35,7 +36,9 @@ public class PanelDescription extends JPanel {
 
 
 		textArea=new JTextArea();
-		textArea.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/5));
+		textArea.setPreferredSize(new Dimension(Window.outil.getScreenSize().width-500,Window.outil.getScreenSize().height/6));
+		textArea.setEditable(false);
+		textArea.setBackground(new Color(230,230,230));
 
 		label=new JLabel("Description de l'objet "+nomObjet+ " : ");
 		label.setPreferredSize(new Dimension(70,30));
@@ -44,12 +47,15 @@ public class PanelDescription extends JPanel {
 		panelBouton.setLayout(new GridLayout(3,1,0,20));
 
 		valider=new JButton("Valider");
-		supprimer=new JButton("Supprimer");
-		annuler=new JButton("Annuler");
+		valider.setEnabled(false);
+		modifier=new JButton("Modifier");
 
+		valider.addMouseListener(this);
+		modifier.addMouseListener(this);
+
+
+		panelBouton.add(modifier);
 		panelBouton.add(valider);
-		panelBouton.add(supprimer);
-		panelBouton.add(annuler);
 		panelBouton.setBackground(new Color(215,215,215));
 		/*
 		panelDescription=new JPanel();
@@ -60,6 +66,44 @@ public class PanelDescription extends JPanel {
 
 		this.add(textArea);
 		this.add(panelBouton);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if(e.getSource().equals(modifier)){
+			textArea.setEditable(true);
+			valider.setEnabled(true);
+			modifier.setEnabled(false);
+			textArea.setBackground(new Color(255,255,255));
+		}
+
+
+		if(e.getSource().equals(valider)){
+			textArea.setEditable(false);
+			valider.setEnabled(false);
+			modifier.setEnabled(true);
+			textArea.setBackground(new Color(230,230,230));
+		}
+
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
