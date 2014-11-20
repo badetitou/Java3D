6b3bc6e1ebd03c15e60a436574d5b4aa799a239e
Loïc <Loc@TYPICAL-PC>
2014-11-20 21:02:14 +0100

    ModeSquelette implemented à toi de jouer badetitou :)

--- Java3D/src/fr/view/BarreVerticale.java
+++ Java3D/src/fr/view/BarreVerticale.java
@@ -22,39 +22,47 @@ public class BarreVerticale extends JPanel implements MouseListener {
 	private final JButton b1;
 	private final JButton b2;
 	private final JButton b3;
+	private final JButton b4;
 
 	private final ImageIcon ic1;
 	private final ImageIcon ic2;
 	private final ImageIcon ic3;
+	private final ImageIcon ic4;
 
 	private final ImageIcon ic6;
 	private final ImageIcon ic7;
 	private final ImageIcon ic8;
+	private final ImageIcon ic9;
 
 	private boolean bb1 = true;
 	private boolean bb2 = false;
 	private boolean modeEdit = false;
+	private boolean modeSquelette=false;
 	private final MyDeskTopPane dp;
 
 	public BarreVerticale(MyDeskTopPane dp) {
 		this.dp = dp;
-		this.setLayout(new GridLayout(3, 1, 0, 4));
+		this.setLayout(new GridLayout(4, 1, 0, 4));
 		this.setBorder(BorderFactory.createLoweredBevelBorder());
 		this.setBackground(new Color(190, 190, 190));
 
 		b1 = new JButton();
 		b2 = new JButton();
 		b3 = new JButton();
+		b4 = new JButton();
 
 		b1.setToolTipText("Mode 1 vue");
 		b2.setToolTipText("Mode 4 vues");
 		b3.setToolTipText("Mode edit");
+		b4.setToolTipText("Mode squelette");
 
 		ic1 = new ImageIcon(new ImageIcon("ressources/icones/oneview.png")
 		.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH));
 		ic2 = new ImageIcon(new ImageIcon("ressources/icones/multipanel.png")
 		.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH));
 		ic3 = new ImageIcon(new ImageIcon("ressources/icones/edit.png")
+		.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH));
+		ic4=new ImageIcon(new ImageIcon("ressources/icones/squelette.png")
 		.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH));
 
 		ic6 = new ImageIcon(new ImageIcon("ressources/icones/oneviewclic.png")
@@ -64,27 +72,34 @@ public class BarreVerticale extends JPanel implements MouseListener {
 				.getScaledInstance(60, 60, Image.SCALE_SMOOTH));
 		ic8 = new ImageIcon(new ImageIcon("ressources/icones/editclic.png")
 		.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH));
+		ic9 =new ImageIcon(new ImageIcon("ressources/icones/squeletteclic.png")
+		.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH));
 
 		b1.setMargin(new Insets(0, 0, 0, 0));
 		b2.setMargin(new Insets(0, 0, 0, 0));
 		b3.setMargin(new Insets(0, 0, 0, 0));
+		b4.setMargin(new Insets(0, 0, 0, 0));
 
 		b1.setBorder(null);
 		b2.setBorder(null);
 		b3.setBorder(null);
+		b4.setBorder(null);
 
 		b1.addMouseListener(this);
 		b2.addMouseListener(this);
 		b3.addMouseListener(this);
+		b4.addMouseListener(this);
 
 		b1.setIcon(ic6);
 		bb1 = true;
 		b2.setIcon(ic2);
 		b3.setIcon(ic3);
+		b4.setIcon(ic4);
 
 		this.add(b1);
 		this.add(b2);
 		this.add(b3);
+		this.add(b4);
 
 	}
 
@@ -93,6 +108,8 @@ public class BarreVerticale extends JPanel implements MouseListener {
 			b1.setIcon(ic6);
 			b2.setIcon(ic2);
 			b3.setIcon(ic3);
+			b4.setIcon(ic4);
+			modeSquelette=false;
 			bb1 = true;
 			bb2 = false;
 			modeEdit = false;
@@ -110,9 +127,11 @@ public class BarreVerticale extends JPanel implements MouseListener {
 			b2.setIcon(ic7);
 			b1.setIcon(ic1);
 			b3.setIcon(ic3);
+			b4.setIcon(ic4);
 			bb2 = true;
 			bb1 = false;
 			modeEdit = false;
+			modeSquelette=false;
 			dp.getPanel().setD(new Dimension(MyDeskTopPane.dimmini)); // NE PAS
 			// TOUCHER
 			// CECI
@@ -131,9 +150,11 @@ public class BarreVerticale extends JPanel implements MouseListener {
 			b3.setIcon(ic8);
 			b2.setIcon(ic2);
 			b1.setIcon(ic1);
+			b4.setIcon(ic4);
 			bb1 = false;
 			bb2 = false;
 			modeEdit = true;
+			modeSquelette=false;
 			dp.getPanel().setD(new Dimension(MyDeskTopPane.dimension));
 			this.dp.getiFrameMain().setPreferredSize(MyDeskTopPane.dimension);
 			this.dp.getiFrameDessous().setVisible(false);
@@ -144,6 +165,17 @@ public class BarreVerticale extends JPanel implements MouseListener {
 			 */
 			this.dp.getModel().zoomAuto(MyDeskTopPane.dimension);
 
+		}
+
+		else if (e.getSource().equals(b4) && !modeSquelette){
+			b3.setIcon(ic3);
+			b2.setIcon(ic2);
+			b1.setIcon(ic1);
+			b4.setIcon(ic9);
+			bb1 = false;
+			bb2 = false;
+			modeEdit = false;
+			modeSquelette=true;
 		}
 
 		this.dp.getPanel().repaint();
