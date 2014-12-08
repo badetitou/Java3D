314e3419c1825d97e7bb2ef2c506b61b54ffc464
Benoit <badetitou@gmail.com>
2014-12-08 19:58:27 +0100

    In Work

--- Java3D/src/fr/model/ReadText.java
+++ Java3D/src/fr/model/ReadText.java
@@ -14,7 +14,7 @@ import javax.swing.JOptionPane;
 
 /**
  * @author Loïc Cette classe a pour but de lire un fichier texte contenant le
- *         code d'un objet et de le parser puis de le ranger dans une ArrayList.
+ *         code d'un objet et de le parser puis de le ranger dans une ArrayList
  */
 public class ReadText {
 
